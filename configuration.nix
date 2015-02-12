87517bf88fa41b72daa5cc132313238709b035b4
Profpatsch <mail@profpatsch.de>
2015-02-12 18:06:01 +0100

    All necessary packages.

--- configuration.nix
+++ configuration.nix
@@ -2,13 +2,22 @@
 
 {
   imports = [ <nixos/modules/installer/virtualbox-demo.nix> ];
-
-  environment.systemPackages = with pkgs; [
-    emacs
-    fish
-    git
-    guile
-    tmux
-    vim
-  ];
+  
+  environment.systemPackages = with pkgs;
+    let
+      haskPkgs = with haskellPackages [
+        ghc
+        mtl
+        network
+        cabal-install
+      ]
+    in [
+      emacs
+      fish
+      git
+      tmux
+      vim
+      maven
+      jdk
+    ] ++ haskPkgs;
 }
